608e913bab022f80613d44bef53e14b0229b8320
Lloyd Carie Mendoza Obligado <lobligado@intercommerce.com.ph>
2023-06-01 21:54:08 +0800

    async/await and error handling

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,23 +3,42 @@ import BlogList from "./blog-list";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null)
+  const [isPending, setIsPending] = useState(true)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
-    const Blogs = async () => {
-        const res  = await fetch("http://localhost:8000/blogs")
-        const data = await res.json()
-        setBlogs(data)
-        }
+    setTimeout(() => {
+      const Blogs = async () => {
+        try {
+          const res  = await fetch("http://localhost:8000/blogs")
 
-        Blogs()
-
-        return () => {
-    
+          if (!res.ok) {
+            throw new Error('Could not fetch the data for that resource')
+          }
+          
+          const data = await res.json()
+          setBlogs(data)
+          setError(null)
+        } catch (err) { 
+          setIsPending(false)
+          setError(err.message)
+        } finally {
+          setIsPending(false)
         }
+      }
+
+      Blogs()
+
+      return () => {
+
+      }
+    }, 1000)
     }, [])
 
   return (
     <div className="home">
+        {error && <div>{error}</div>}
+        {isPending && <div>Loading...</div> }
         {blogs && <BlogList blogs={blogs} title="All Blogs!" /> }
     </div>
   );
