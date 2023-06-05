4b24146be59edf3b5034aee9afe252e8a8f45277
Lloyd Carie Mendoza Obligado <lobligado@intercommerce.com.ph>
2023-06-05 20:42:08 +0800

    reusing custom hooks

--- src/components/blog-details.jsx
+++ src/components/blog-details.jsx
@@ -1,11 +1,21 @@
 import { useParams } from 'react-router-dom'
+import useFetch from '../hooks/useFetch'
 
 const BlogDetails = () => {
   const { id } = useParams()
-  
+  const { data: blog, error, isPending } = useFetch(' http://localhost:8000/blogs/' + id)
+
   return (
     <div className="blog-details">
-        <h2>Blog Deatils - { id }</h2>
+        { isPending && <div>Loading...</div>}
+        { error && <div> { error } </div> }
+        { blog && (
+          <article>
+            <h2>{ blog.title }</h2>
+            <p>{ blog. author }</p>
+            <div>{ blog.body }</div>
+          </article>
+        )}
     </div>
   )
 }
